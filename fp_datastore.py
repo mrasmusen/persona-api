e916a5ec58828beacbff998b4d8c33cef89a1259
Max Rasmusen <maxrasmusen11742@gmail.com>
2019-01-28 18:26:42 +0000

    added skeleton for fake persona server

---
+++ fp_datastore.py
@@ -0,0 +1,50 @@
+"""
+  Abstract class FakeProfileDataStore defines the three methods supported
+  by a fake profiles data store. 
+  Also defines some exceptions to be user by fake profiles data stores.
+"""
+
+from abc import ABC, abstractmethod
+
+class FakeProfilesDataStore(ABC):
+
+  """
+    Searches the datastore with a user with a given username.
+    Returns Json data for that user if it exists, raises a
+    ResourceNotFoundError if it doesn't.
+  """
+  @abstractmethod
+  def get_single_user(self, username):
+    pass
+
+  """
+    Returns all users within a given range, defined by start_index
+    and page_size, in the datastore. Doesn't specify how the results
+    are to be sorted, but the sorting must be consistent across 
+    multiple calls. 
+    Raises a DataOutOfRangeError if the supplied page boundaries
+    are out of the range of the data.
+  """
+  @abstractmethod
+  def get_all_users(self, start_index, page_size):
+    pass
+  
+  """
+    Deletes a user with the given username. 
+    Raises a ResourceNotFoundError if the user doesn't exist.
+  """
+  @abstractmethod
+  def delete_single_user(self, username):
+    pass
+  
+class NoDataStoreError(Exception):
+  pass
+
+class ResourceNotFoundError(Exception):
+  pass
+
+class UnknownDataStoreError(Exception):
+  pass
+
+class DataOutOfRangeError(Exception):
+  pass
---
+++ fp_server.py
@@ -0,0 +1,58 @@
+import falcon
+import json
+import time
+
+from fp_dummy_datastore import DummyFPDataStore
+from fp_datastore import FakeProfilesDataStore, ResourceNotFoundError, DataOutOfRangeError
+
+class AllUsersResource(object):
+	def __init__(self, datastore):
+		self.datastore = datastore
+	
+	def on_get(self, req, resp):
+		try:
+			"""
+				Parse the parameters, getting start and pagesize to get all users.
+			"""
+			start_index = int(req.params["start"])
+			page_size = int(req.params["pagesize"])
+			resp.body = json.dumps(self.datastore.get_all_users(start_index, page_size))
+			resp.content_type = falcon.MEDIA_JSON
+			resp.status = falcon.HTTP_200
+		except DataOutOfRangeError:
+			resp.status = falcon.HTTP_404
+			resp.body = "Data out of range!"
+		except (KeyError, ValueError):
+			resp.status = falcon.HTTP_400
+			resp.body = "Required parameters: start<int> and pagesize<int>"
+
+class SingleUserResource(object):
+	def __init__(self, datastore):
+		self.datastore = datastore
+
+	def on_get(self, req, resp, username):
+		try:
+			resp.status = falcon.HTTP_200
+			resp.content_type = falcon.MEDIA_JSON
+			resp.body = json.dumps(self.datastore.get_single_user(username))
+		except ResourceNotFoundError:
+			resp.status = falcon.HTTP_404
+			resp.body = "User not found."
+
+	def on_delete(self, req, resp, username):
+		try:
+			resp.status = falcon.HTTP_200
+			self.datastore.delete_single_user(username)
+			resp.body = "Deleted successfully."
+		except ResourceNotFoundError:
+			resp.status = falcon.HTTP_404
+			resp.body = "User not found."
+		
+ds = DummyFPDataStore()
+app = falcon.API()
+
+all_users_resourse = AllUsersResource(ds)
+single_user_resource = SingleUserResource(ds)
+
+app.add_route("/users", all_users_resourse,)
+app.add_route("/users/{username}", single_user_resource)
