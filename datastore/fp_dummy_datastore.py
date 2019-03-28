1903fead151c82128aed3d4c3865c64834ad6cdd
Max Rasmusen <mrasmusen@caci.co.uk>
2019-03-28 11:29:56 +0000

    re-formatted for pep8

--- datastore/fp_dummy_datastore.py
+++ datastore/fp_dummy_datastore.py
@@ -1,51 +1,52 @@
 """
-  Dummy implementation of FakeProfilesDataStore used to test
-  the routing in the server. No storage takes place, data is
-  generated on the spot. Some values can cause the dummy store
-  to raise errors. The schema is arbitrary and isn't related 
-  to the actual data the fake profiles use.
+    Dummy implementation of FakeProfilesDataStore used to test
+    the routing in the server. No storage takes place, data is
+    generated on the spot. Some values can cause the dummy store
+    to raise errors. The schema is arbitrary and isn't related
+    to the actual data the fake profiles use.
 """
-from datastore.fp_datastore import FakeProfilesDataStore, ResourceNotFoundError, DataOutOfRangeError
+from datastore.fp_datastore import FakeProfilesDataStore, \
+    ResourceNotFoundError, DataOutOfRangeError
+
 
 class DummyFPDataStore(FakeProfilesDataStore):
-  def __init__(self):
-    pass
-  
-  """
-    Returns successfully a dict with data in it. 
-    If the username is 'notfound' then the function
-    will raise a ResourceNotFoundError.
-  """
-  def get_single_user(self, username):
-    if username == "notfound":
-      raise ResourceNotFoundError()
-    else:
-      return {"username": username, "pet": "Dog"}
+    def __init__(self):
+        pass
 
+    """
+        Returns successfully a dict with data in it.
+        If the username is 'notfound' then the function
+        will raise a ResourceNotFoundError.
+    """
+    def get_single_user(self, username):
+        if username == "notfound":
+            raise ResourceNotFoundError()
+        else:
+            return {"username": username, "pet": "Dog"}
 
-  """
-    Returns users with username 'fake_person_X' where
-    x is their index in the database. If the index is
-    over 99 then raise DataOutOfRangeError.
-  """
-  def get_all_users(self, start_index, page_size):
-    if start_index + page_size >= 100:
-      raise DataOutOfRangeError()
-    else:
-      users = []
-      for i in range(page_size):
-        users.append({
-          "username": "fake_person_{}".format(i + start_index),
-          "pet": "Cat"})
-      return users
-  
-  """
-    If username is 'notfound', return a ResourceNotFoundError. 
-    Otherwise do nothing.
-  """
-  def delete_single_user(self, username):
-    if username == "notfound":
-      raise ResourceNotFoundError()
-  
-  def add_data_from_json_file(self, jsonfile):
-    pass
+    """
+        Returns users with username 'fake_person_X' where
+        x is their index in the database. If the index is
+        over 99 then raise DataOutOfRangeError.
+    """
+    def get_all_users(self, start_index, page_size):
+        if start_index + page_size >= 100:
+            raise DataOutOfRangeError()
+        else:
+            users = []
+        for i in range(page_size):
+            users.append({
+                "username": "fake_person_{}".format(i + start_index),
+                "pet": "Cat"})
+        return users
+
+    """
+        If username is 'notfound', return a ResourceNotFoundError.
+        Otherwise do nothing.
+    """
+    def delete_single_user(self, username):
+        if username == "notfound":
+            raise ResourceNotFoundError()
+
+    def add_data_from_json_file(self, jsonfile):
+        pass
--- datastore/fp_sql_datastore.py
+++ datastore/fp_sql_datastore.py
@@ -1,146 +1,150 @@
 """
-  Implementation of FakeProfilesDataStore using sqlite3 to create a local
-  database. This allows us to abstract away the actual data access and 
-  don't need to worry about altering a large json file.
+    Implementation of FakeProfilesDataStore using sqlite3 to create a local
+    database. This allows us to abstract away the actual data access and
+    don't need to worry about altering a large json file.
 
-  When the class is created, any existing database is cleared and the
-  caller can then load in new json from the zip file.
+    When the class is created, any existing database is cleared and the
+    caller can then load in new json from the zip file.
 
-  The database has 2 tables - one for users and a second for websites, 
-  since the websites attribute has a variable length between users.
+    The database has 2 tables - one for users and a second for websites,
+    since the websites attribute has a variable length between users.
 """
 
 import os
 import mysql.connector
 import traceback
 
-from datastore.fp_datastore import FakeProfilesDataStore, ResourceNotFoundError, DataOutOfRangeError, NoDataStoreError
+from datastore.fp_datastore import FakeProfilesDataStore, \
+    ResourceNotFoundError, DataOutOfRangeError, NoDataStoreError
+
 
 class SqlDataStore(FakeProfilesDataStore):
-  def __init__(self):
-    self.db_conn_data = None
-    try:
-      self.db_conn_data = {
-        "user": os.getenv("MYSQL_SERVICE_USER", "persona_user"),
-        "password": os.getenv("MYSQL_SERVICE_PASSWORD", "password"),
-        "host": os.getenv("MYSQL_SERVICE_HOST", "localhost"),
-        "database": os.getenv("MYSQL_SERVICE_PERSONA_DB", "personadb")
-      }
-    except Exception:
-      traceback.print_exc()
-      self.db_conn_data = None
-    
-  def get_single_user(self, username):
-    if self.db_conn_data is None:
-      raise NoDataStoreError
-      
+    def __init__(self):
+        self.db_conn_data = None
+        try:
+            self.db_conn_data = {
+                "user": os.getenv("MYSQL_SERVICE_USER", "persona_user"),
+                "password": os.getenv("MYSQL_SERVICE_PASSWORD", "password"),
+                "host": os.getenv("MYSQL_SERVICE_HOST", "localhost"),
+                "database": os.getenv("MYSQL_SERVICE_PERSONA_DB", "personadb")
+            }
+        except Exception:
+            traceback.print_exc()
+            self.db_conn_data = None
+
+    def get_single_user(self, username):
+        if self.db_conn_data is None:
+            raise NoDataStoreError
+
+        db = mysql.connector.connect(**self.db_conn_data)
+        cursor = db.cursor()
+
+        cursor.execute("""
+                SELECT * from users WHERE username=%s;
+            """, (username, ))
+
+        """
+            Need to get the websites data in separate calls, since
+            sqlite3 doesn't support FULL OUTER JOINs in python.
+        """
+        users_data = cursor.fetchall()
+        websites_data = {}
+        for user in users_data:
+            cursor.execute("""
+                SELECT website from websites WHERE user_id=%s;
+            """, (user[0], ))
+
+            websites_data[user[0]] = cursor.fetchall()
+
+        return self.format_rows_into_json(users_data, websites_data)
+
+    def get_all_users(self, start_index, page_size):
+        if self.db_conn_data is None:
+            raise NoDataStoreError
+
     db = mysql.connector.connect(**self.db_conn_data)
     cursor = db.cursor()
-    
+
     cursor.execute("""
-      SELECT * from users WHERE username=%s;
-    """, (username, ))
+        SELECT * from users WHERE id>=%s AND id<%s;
+        """, (start_index, start_index + page_size))
 
     """
-      Need to get the websites data in separate calls, since sqlite3 doesn't support
-      FULL OUTER JOINs in python.
+      Need to get the websites data in separate calls, since
+      sqlite3 doesn't support FULL OUTER JOINs in python.
     """
     users_data = cursor.fetchall()
     websites_data = {}
     for user in users_data:
-      cursor.execute("""
-        SELECT website from websites WHERE user_id=%s;
-      """, (user[0], ))
+        # print(user)
+        cursor.execute("""
+            SELECT website from websites WHERE user_id=%s;
+        """, (user[0], ))
 
-      websites_data[user[0]] = cursor.fetchall()
+        websites_data[user[0]] = cursor.fetchall()
 
-    return self.format_rows_into_json(users_data, websites_data)
-  
-  def get_all_users(self, start_index, page_size):
-    if self.db_conn_data is None:
-      raise NoDataStoreError
-      
-    db = mysql.connector.connect(**self.db_conn_data)
-    cursor = db.cursor()
-    
-    cursor.execute("""
-      SELECT * from users WHERE id>=%s AND id<%s;
-    """, (start_index, start_index + page_size))
+        return self.format_rows_into_json(users_data, websites_data)
 
-    """
-      Need to get the websites data in separate calls, since sqlite3 doesn't support
-      FULL OUTER JOINs in python.
-    """
-    users_data = cursor.fetchall()
-    websites_data = {}
-    for user in users_data:
-      # print(user)
-      cursor.execute("""
-        SELECT website from websites WHERE user_id=%s;
-      """, (user[0], ))
-
-      websites_data[user[0]] = cursor.fetchall()
-
-    return self.format_rows_into_json(users_data, websites_data)
-
-  def delete_single_user(self, username):
-    if self.db_conn_data is None:
-      raise NoDataStoreError
+    def delete_single_user(self, username):
+        if self.db_conn_data is None:
+            raise NoDataStoreError
 
     db = mysql.connector.connect(**self.db_conn_data)
     cursor = db.cursor()
-    
+
     cursor.execute("""
-      DELETE FROM users WHERE username=%s
-    """, (username, ))
+           DELETE FROM users WHERE username=%s
+        """, (username, ))
 
     db.commit()
-  
-  """
-    Takes the raw database output and formats it into the json the user expects.
-  """
-  def format_rows_into_json(self, rows, websites):
-    if self.db_conn_data is None:
-      raise NoDataStoreError
 
-    json_list = []
-    for row in rows:
-      json_list.append(self.format_row_into_json(row, websites[row[0]]))
+    """
+        Takes the raw database output and formats it into the json
+        the user expects.
+    """
 
-    return json_list
-    
-  """
-    Build single json item from raw data.
-  """
-  def format_row_into_json(self, row, websites):
-    if self.db_conn_data is None:
-      raise NoDataStoreError
+    def format_rows_into_json(self, rows, websites):
+        if self.db_conn_data is None:
+            raise NoDataStoreError
 
-    return {
-      "job": row[1],
-      "company": row[2],
-      "ssn": row[3],
-      "residence": row[4],
-      "current_location": [
-        row[5], 
-        row[6]
-      ], 
-      "blood_group": row[7],
-      "website": [w[0] for w in websites], 
-      "username": row[8],
-      "name": row[9],
-      "sex": row[10],
-      "address": row[11],
-      "mail": row[12],
-      "birthdate": row[13]
-    }
-  
-  def add_data_from_json_file(self, jsonfile):
-    pass
-  
-  def __del__(self):
-    if self.db_conn_data is None:
-      raise NoDataStoreError
-      
+        json_list = []
+        for row in rows:
+            json_list.append(self.format_row_into_json(row, websites[row[0]]))
+
+        return json_list
+
+    """
+        Build single json item from raw data.
+    """
+    def format_row_into_json(self, row, websites):
+        if self.db_conn_data is None:
+            raise NoDataStoreError
+
+        return {
+            "job": row[1],
+            "company": row[2],
+            "ssn": row[3],
+            "residence": row[4],
+            "current_location": [
+                row[5],
+                row[6]
+            ],
+            "blood_group": row[7],
+            "website": [w[0] for w in websites],
+            "username": row[8],
+            "name": row[9],
+            "sex": row[10],
+            "address": row[11],
+            "mail": row[12],
+            "birthdate": row[13]
+        }
+
+    def add_data_from_json_file(self, jsonfile):
+        pass
+
+    def __del__(self):
+        if self.db_conn_data is None:
+            raise NoDataStoreError
+
     db = mysql.connector.connect(**self.db_conn_data)
     db.close()
